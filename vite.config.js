daa130fd1602162467bdd59203e8f675ac2e75e4
Yousaf P <yousafpv1054@gmail.com>
2026-02-20 21:50:48 +0530

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/lumid_ai/',        // 👈 ADD THIS LINE
   plugins: [react()],
   publicDir: 'resources',
 })
