a008fbad6594c974c4ff4d88fd45968b3331456c
yousaf p <yousafpv1054@gmail.com>
2026-02-20 22:29:45 +0530

    Refactor: Update public directory to resources and fix configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/lumid_ai/',        // 👈 ADD THIS LINE
   plugins: [react()],
   publicDir: 'resources',
 })
