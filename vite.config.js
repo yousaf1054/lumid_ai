046de283969128846050e7fdad59ae7d5cadf744
Yousaf P <yousafpv1054@gmail.com>
2026-02-20 22:32:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/lumid_ai/',        // ✅ REQUIRED for GitHub Pages
   plugins: [react()],
-  publicDir: 'resources',
+  publicDir: 'resources',   // ✅ for images/static files
 })
